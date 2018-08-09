69cf3e8b64a28b301ad533ad6aed61d3ed2e283d
Mateus Arruda <arrudamateus3@hotmail.com>
2018-08-09 09:25:39 -0300

    first commit
    The app

---
+++ QueueApplication.Common/PriorityQueue.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueApplication.Common
+{
+    public class PriorityQueue<T> : IEnumerable<T>, ICollection where T : IComparable<T>
+    {
+        private LinkedList<T> _list;
+        public PriorityQueue()
+        {
+            _list = new LinkedList<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            if (_list.Count == 0)
+            {
+                _list.AddLast(item);
+            }
+            else
+            {
+                var current = _list.First;
+
+                while (current != null && current.Value.CompareTo(item) > 0)
+                {
+                    current = current.Next;
+                }
+                if (current == null)
+                {
+                    _list.AddLast(item);
+                }
+                else
+                {
+                    _list.AddBefore(current, item);
+                }
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("Nothing to be removed!");
+            }
+            var result = _list.First.Value;
+            _list.RemoveFirst();
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("Nothing to show!");
+            }
+            return _list.First.Value;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public int Count
+        {
+            get
+            { 
+                return _list.Count; 
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            _list
+                .Reverse()
+                .Skip(index)
+                .Reverse()
+                .ToList()
+                .CopyTo(array as T[], 0);
+        }
+
+        public bool IsSynchronized
+        {
+            get { return true; }
+        }
+
+        public object SyncRoot
+        {
+            get { return this; }
+        }
+    }
+}
---
+++ QueueApplication/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using QueueApplication.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QueueApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Random _rnd = new Random();
+        PriorityQueue<int> _queue = new PriorityQueue<int>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _queue.Enqueue(_rnd.Next(0, 1000));
+            UpdateGrid();
+        }
+
+        private void btnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            if (_queue.Count > 0)
+            {
+                listNumbers.Items.Add(_queue.Dequeue());
+                UpdateGrid();
+            }
+        }
+
+
+        private void UpdateGrid()
+        {
+            label1.Content = "";
+            label2.Content = "";
+            label3.Content = "";
+            label4.Content = "";
+            label5.Content = "";
+            label6.Content = "";
+            label7.Content = "";
+            label8.Content = "";
+
+            var index = 1;
+            foreach (var message in _queue)
+            {
+                switch (index)
+                {
+                    case 1:
+                        label1.Content = message;
+                        break;
+                    case 2:
+                        label2.Content = message;
+                        break;
+                    case 3:
+                        label3.Content = message;
+                        break;
+                    case 4:
+                        label4.Content = message;
+                        break;
+                    case 5:
+                        label5.Content = message;
+                        break;
+                    case 6:
+                        label6.Content = message;
+                        break;
+                    case 7:
+                        label7.Content = message;
+                        break;
+                    case 8:
+                        label8.Content = message;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                index++;
+
+                if (index > 8)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
